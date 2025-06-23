39aae331414252724063fbc3e37db33e5e1c2712
Rely Ky <relyky@hotmail.com>
2025-06-23 16:20:20 +0800

    setup @mui7

--- src/app/demo/demo002/page.tsx
+++ src/app/demo/demo002/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default async function Demo002Page() {
 
@@ -7,7 +8,8 @@ export default async function Demo002Page() {
       <h1>Demo002Page</h1>
       <p>今天天氣真好。</p>
 
-      <Button>Button</Button>
+      <MenuIcon />
+      <Button variant='contained' color='primary'>按鈕</Button>
 
     </div>
   )
