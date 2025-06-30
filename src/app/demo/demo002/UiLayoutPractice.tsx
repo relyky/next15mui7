21ab5510467fa60a3eb8b75d839581d6d3dc2c29
Rely Ky <relyky@hotmail.com>
2025-06-30 09:28:30 +0800

    StackLab

--- src/app/demo/demo002/UiLayoutPractice.tsx
+++ src/app/demo/demo002/UiLayoutPractice.tsx
@@ -1,7 +1,7 @@
-import { Box, Paper, Stack, styled, Typography } from "@mui/material";
+import { Box, Paper, Slider, Stack, styled, Typography } from "@mui/material";
 import SimpleRadioField from "./widgets/SimpleRadioField";
-import { useState } from "react";
-import { Grid, ResponsiveStyleValue, SxProps, SystemProps } from '@mui/system';
+import { FC, useMemo, useState } from "react";
+import { Grid } from '@mui/system';
 
 export default function UiLayoutPractice() {
   return (
@@ -9,12 +9,11 @@ export default function UiLayoutPractice() {
       <Typography variant="h3" gutterBottom>使用介面部局</Typography>
 
       <Box component='p' typography='body1'>
+        參考：Border、Stack、Grid。<br/>
         [Stack](https://mui.com/system/react-stack/)<br />
         [Grid](https://mui.com/material-ui/react-grid/)<br />
         [Border](https://mui.com/system/borders/)
       </Box>
-
-      <Box typography='body1'>參考：Border、Stack、Grid。</Box>
 
       <StackLab />
 
@@ -41,10 +40,21 @@ function StackLab() {
   const [alignItems, setAlignItems] = useState<string>('flex-start');
   const [spacing, setSpacing] = useState<string>('0')
   const [gap, setGap] = useState<string>('0')
+  const [count, setCount] = useState<number>(3)
+
+  const countList = useMemo(() =>
+    Array.from({ length: count })
+      .map((_, i) => i + 1)
+    , [count]);
 
   return (
     <div>
+      <Typography variant="h6">Stack Lab</Typography>
+      <Box typography='body1'>其 CSS display 皆是 'flex'。</Box>
       <Grid container>
+        <Grid size={{ xs: 12 }}>
+          <CountSlider count={count} onChange={setCount} />
+        </Grid>
         <Grid size={{ xs: 12, md: 6 }}>
           <SimpleRadioField label='direction' value={direction} onChange={setDirection}
             optionList={['row', 'row-reverse', 'column', 'column-reverse']} />
@@ -52,6 +62,14 @@ function StackLab() {
         <Grid size={{ xs: 12, md: 6 }}>
           <SimpleRadioField label='flexWrap' value={flexWrap} onChange={setFlexWrap}
             optionList={['nowrap', 'wrap', 'wrap-reverse']} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <SimpleRadioField label='spacing' value={spacing} onChange={setSpacing}
+            optionList={['0', '0.5', '1', '2', '3', '4', '8', '12']} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <SimpleRadioField label='gap' value={gap} onChange={setGap}
+            optionList={['0', '0.5', '1', '2', '3', '4', '8', '12']} />
         </Grid>
         <Grid size={{ xs: 12 }}>
           <SimpleRadioField label='justifyContent' value={justifyContent} onChange={setJustifyContent}
@@ -64,14 +82,6 @@ function StackLab() {
         <Grid size={{ xs: 12 }}>
           <SimpleRadioField label='alignItems' value={alignItems} onChange={setAlignItems}
             optionList={['flex-start', 'center', 'flex-end', 'stretch', 'baseline']} />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <SimpleRadioField label='spacing' value={spacing} onChange={setSpacing}
-            optionList={['0', '0.5', '1', '2', '3', '4', '8', '12']} />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <SimpleRadioField label='gap' value={gap} onChange={setGap}
-            optionList={['0', '0.5', '1', '2', '3', '4', '8', '12']} />
         </Grid>
       </Grid>
 
@@ -88,15 +98,73 @@ function StackLab() {
           minHeight: 400,
         }}>
 
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((v, i) => (
+        {countList.map((v, i) => (
           <Item key={v}>
             <Box sx={{ py: `${v * 4}px`, textAlign: 'center' }}>
               {`Item ${v}`}
             </Box>
           </Item>
         ))}
+
+        {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((v, i) => (
+          <Item key={v}>
+            <Box sx={{ py: `${v * 4}px`, textAlign: 'center' }}>
+              {`Item ${v}`}
+            </Box>
+          </Item>
+        ))} */}
       </Stack>
     </div>
 
   )
 }
+
+//-----------------------------------------------------------------------------
+
+const CountSlider: FC<{
+  count: number,
+  onChange: (newCount: number) => void
+}> = (props) => {
+  //const [count, setCount] = useState<number>(3);
+
+  const handleChange = (event: Event, newValue: number) => {
+    props.onChange(newValue);
+    //setCount(newValue);
+  };
+
+  return (
+    <Box sx={{ width: 250 }}>
+      <Typography gutterBottom>
+        排列項目數量: {props.count}
+      </Typography>
+      <Slider
+        value={props.count}
+        min={3}
+        step={1}
+        max={20}
+        marks={marks}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+      />
+    </Box>
+  );
+}
+
+const marks = [
+  {
+    value: 3,
+    label: '3',
+  },
+  {
+    value: 7,
+    label: '7',
+  },
+  {
+    value: 13,
+    label: '13',
+  },
+  {
+    value: 20,
+    label: '20',
+  },
+];
