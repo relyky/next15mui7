1a0bd257e48e42f28a1dd392d5d24a06c18a11c8
Rely Ky <relyky@hotmail.com>
2025-08-30 13:56:34 +0800

    fix

--- src/app/demo/demo005/CountUpDemo.tsx
+++ src/app/demo/demo005/CountUpDemo.tsx
@@ -1,10 +1,6 @@
 "use client"
 import { useEffect, useRef, useState } from 'react'
 import { Box } from '@mui/material';
-<<<<<<< HEAD
-=======
-import { useEffect, useId, useMemo, useState } from 'react'
->>>>>>> 5719fbc84507bfbd9f6f8f796d6df9aad0ebaa0e
 import { useCountUp } from 'react-countup';
 
 export default function CountUpDemo() {
@@ -34,24 +30,14 @@ export default function CountUpDemo() {
 const CountUpWidget: React.FC<{
   end: number
 }> = ({ end }) => {
-<<<<<<< HEAD
   const countUpRef = useRef<HTMLDivElement | null>(null);
   const hand = useCountUp({ ref: countUpRef as any, end: end });
-=======
-  const id = useId()
-  const counterId = useMemo(() => `up-counter-${id}`, [id])
-  const hand = useCountUp({ ref: counterId, end: end });
->>>>>>> 5719fbc84507bfbd9f6f8f796d6df9aad0ebaa0e
 
   useEffect(() => {
     hand.update(end)
   }, [end])
 
   return (
-<<<<<<< HEAD
     <Box component='span' ref={countUpRef} sx={{ fontSize: '2em' }} />
-=======
-    <Box component='span' id={counterId} sx={{ fontSize: '2em' }} />
->>>>>>> 5719fbc84507bfbd9f6f8f796d6df9aad0ebaa0e
   )
 }
