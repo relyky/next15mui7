0df4b0420d761624a37d02e0cf643b2141cdeb4c
Rely Ky <relyky@hotmail.com>
2025-08-30 10:54:12 +0800

    count-up demo

---
+++ src/app/demo/demo005/loading.tsx
@@ -0,0 +1,10 @@
+import {Box, LinearProgress, Typography}  from '@mui/material'
+
+export default function Loading() {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <LinearProgress />
+      <Typography variant='h4'>Demo005 專屬的 Loading...</Typography>
+    </Box>
+  );
+}
--- src/app/demo/demo005/page.tsx
+++ src/app/demo/demo005/page.tsx
@@ -1,12 +1,13 @@
 import { Alert, Box, Button, Container, Typography } from "@mui/material";
 import { getInitData } from "./actions";
+import CountUpDemo from "./CountUpDemo";
 
 export default async function Demo001Page() {
   const initData = await getInitData();
 
   return (
     <Container>
-      <Typography variant="h3">Demo001</Typography>
+      <Typography variant="h3">Demo005 Count Up</Typography>
       <Box typography="body1">next.js 為 SSR/MPA 網站。<br />
         此畫面伺服器端生成後才送下來。<br />
       </Box>
@@ -15,7 +16,7 @@ export default async function Demo001Page() {
         initData: {initData.message}
       </Alert>
 
-      <Button variant='contained' color='primary'>按鈕(無作用)</Button>
+      <CountUpDemo />
 
     </Container>
   )
