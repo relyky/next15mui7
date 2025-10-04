6fb078097434aa7673b9fd56030a98c006256408
Rely Ky <relyky@hotmail.com>
2025-10-04 18:13:41 +0800

    ActionStateLab4

--- src/app/lab/lab01/page.tsx
+++ src/app/lab/lab01/page.tsx
@@ -2,16 +2,18 @@ import { Box, Container, Typography } from "@mui/material";
 import ActionStateLab1 from "./ActionStateLab1";
 import ActionStateLab2 from "./ActionStateLab2";
 import ActionStateLab3 from "./ActionStateLab3";
+import ActionStateLab4 from "./ActionStateLab4";
 
 export default async function Lab01Page() {
   return (
     <Container>
       <Typography variant="h3">Lab01 - useActionState</Typography>
 
-      <Box display='flex' gap={3}>
+      <Box display='flex' flexWrap='wrap' gap={3}>
         <ActionStateLab1 />
         <ActionStateLab2 />
         <ActionStateLab3 />
+        <ActionStateLab4 />
       </Box>
     </Container>
   )
